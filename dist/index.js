e747becd1b99d940fd96a8c507fbb33941f435a4
Mario Kozjak <kozjakm1@gmail.com>
2020-04-03 15:19:42 +0200

    build
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- dist/index.js
+++ dist/index.js
@@ -27,30 +27,46 @@ var _client = _interopRequireDefault(require("./lib/client"));
 
 var _server = _interopRequireDefault(require("./lib/server"));
 
+var __rest = void 0 && (void 0).__rest || function (s, e) {
+  var t = {};
+
+  for (var p in s) {
+    if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0) t[p] = s[p];
+  }
+
+  if (s != null && typeof Object.getOwnPropertySymbols === "function") for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+    if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i])) t[p[i]] = s[p[i]];
+  }
+  return t;
+};
+
 var Client = /*#__PURE__*/function (_CommonClient) {
   (0, _inherits2["default"])(Client, _CommonClient);
 
   function Client() {
     var address = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "ws://localhost:8080";
 
-    var _ref = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-        _ref$autoconnect = _ref.autoconnect,
-        autoconnect = _ref$autoconnect === void 0 ? true : _ref$autoconnect,
-        _ref$reconnect = _ref.reconnect,
-        reconnect = _ref$reconnect === void 0 ? true : _ref$reconnect,
-        _ref$reconnect_interv = _ref.reconnect_interval,
-        reconnect_interval = _ref$reconnect_interv === void 0 ? 1000 : _ref$reconnect_interv,
-        _ref$max_reconnects = _ref.max_reconnects,
-        max_reconnects = _ref$max_reconnects === void 0 ? 5 : _ref$max_reconnects;
+    var _a = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
     var generate_request_id = arguments.length > 2 ? arguments[2] : undefined;
     (0, _classCallCheck2["default"])(this, Client);
-    return (0, _possibleConstructorReturn2["default"])(this, (0, _getPrototypeOf2["default"])(Client).call(this, _websocket["default"], address, {
+
+    var _a$autoconnect = _a.autoconnect,
+        autoconnect = _a$autoconnect === void 0 ? true : _a$autoconnect,
+        _a$reconnect = _a.reconnect,
+        reconnect = _a$reconnect === void 0 ? true : _a$reconnect,
+        _a$reconnect_interval = _a.reconnect_interval,
+        reconnect_interval = _a$reconnect_interval === void 0 ? 1000 : _a$reconnect_interval,
+        _a$max_reconnects = _a.max_reconnects,
+        max_reconnects = _a$max_reconnects === void 0 ? 5 : _a$max_reconnects,
+        rest_options = __rest(_a, ["autoconnect", "reconnect", "reconnect_interval", "max_reconnects"]);
+
+    return (0, _possibleConstructorReturn2["default"])(this, (0, _getPrototypeOf2["default"])(Client).call(this, _websocket["default"], address, Object.assign({
       autoconnect: autoconnect,
       reconnect: reconnect,
       reconnect_interval: reconnect_interval,
       max_reconnects: max_reconnects
-    }, generate_request_id));
+    }, rest_options), generate_request_id));
   }
 
   return Client;
